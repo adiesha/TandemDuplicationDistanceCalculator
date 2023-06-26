a969e583f768e0fcb64bd8e47a7e12f13f3d6de5
Adiesha <adiesha@gmail.com>
2023-06-26 17:11:05 -0600

    Added basic unlimited window LZ77 compression

--- TandemDuplicationDistanceCalculator/src/main/java/org/msu/adiesha/LZ77UnW.java
+++ TandemDuplicationDistanceCalculator/src/main/java/org/msu/adiesha/LZ77UnW.java
@@ -1,10 +1,72 @@
 package org.msu.adiesha;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LZ77UnW {
-    private String input;
+    private String T;
+    private Map<String, Integer> stringMap;
+    private StringBuilder initialBuffer;
 
 
-    public void compress(String input, int buffersize) {
+    private List<TupleO> output;
 
+    static class TupleO {
+        int offset = 0;
+        int size = 0;
+        char c;
+
+        @Override
+        public String toString() {
+            return "TupleO{" +
+                    "offset=" + offset +
+                    ", size=" + size +
+                    ", c=" + c +
+                    '}';
+        }
+    }
+
+    public LZ77UnW(String S, String T) {
+        this.T = T;
+        this.initialBuffer = new StringBuilder();
+        this.initialBuffer.append(S);
+        stringMap = new HashMap<>();
+        this.output = new ArrayList<>();
+    }
+
+    public void compress(String S, String T) {
+        StringBuilder lookaheadbuffer = new StringBuilder();
+        TupleO last = new TupleO();
+        output.add(last);
+        for (char c : T.toCharArray()) {
+            lookaheadbuffer.append(c);
+
+            int index = initialBuffer.indexOf(lookaheadbuffer.toString());
+            if (index == -1) {
+                // does not contain the look ahead buffer in the initial buffer
+                // add char to the initial buffer and remove char from lookahead buffer
+                initialBuffer.append(lookaheadbuffer);
+                lookaheadbuffer.setLength(0);
+                last.c = c;
+                last = new TupleO();
+                this.output.add(last);
+
+            } else {
+                // which means we found a match
+                last.offset = index;
+                last.size = lookaheadbuffer.length();
+
+            }
+        }
+    }
+
+
+    public List<TupleO> getOutput() {
+        for (TupleO t: this.output) {
+            System.out.print(t + "\t");
+        }
+        return this.output;
     }
 }
---
+++ TandemDuplicationDistanceCalculator/src/test/java/org/msu/adiesha/LZ77UnWTest.java
@@ -0,0 +1,21 @@
+package org.msu.adiesha;
+
+import junit.framework.TestCase;
+
+public class LZ77UnWTest extends TestCase {
+
+    LZ77UnW lz77UnW;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        lz77UnW = new LZ77UnW("ACGT", "ACGTAA");
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testCompress() {
+        lz77UnW.compress("ACGT", "ACGTAA");
+        System.out.println(lz77UnW.getOutput());
+    }
+}
