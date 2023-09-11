9792daa751dd26de0a10fcb4f055a3239e997cd6
Adiesha <adiesha@gmail.com>
2023-09-11 11:15:52 -0600

    Added new geneorder util methods

--- TandemDuplicationDistanceCalculator/src/main/java/org/msu/adiesha/utils/ReadMTGeneOrder.java
+++ TandemDuplicationDistanceCalculator/src/main/java/org/msu/adiesha/utils/ReadMTGeneOrder.java
@@ -30,7 +30,7 @@ public class ReadMTGeneOrder {
     public static Map<String, Character> assignCharsToStringList(List<String> list) {
 
         Character[] characters = allowedCharacters.toArray(new Character[allowedCharacters.size()]);
-        ;
+
         int i = 0;
         Map<String, Character> stringCharacterMap = new HashMap<>();
 
@@ -38,7 +38,7 @@ public class ReadMTGeneOrder {
             if (!stringCharacterMap.containsKey(s)) {
                 stringCharacterMap.put(s, characters[i++]);
             } else {
-                System.out.println(s + "was already found");
+                System.out.println(s + " was already found");
             }
         }
 
@@ -46,18 +46,36 @@ public class ReadMTGeneOrder {
     }
 
     public static String getTheChangedOutput(List<String> geneOrder, Map<String, Character> map) {
-        if (geneOrder.size() > map.size()) {
-            System.err.println("Not enough characters");
-            assert false;
-        }
+//        if (geneOrder.size() > map.size()) {
+//            System.err.println("Not enough characters");
+//            assert false;
+//        }
 
         StringBuilder outputBuffer = new StringBuilder();
 
         for (String s : geneOrder) {
             outputBuffer.append(map.get(s));
+            if (!map.containsKey(s)) {
+                System.out.println(s + " is not in the map");
+            }
         }
 
         return outputBuffer.toString();
+
+    }
+
+    public static boolean checkWhetherStringsContainSameCharacters(String str1, String str2) {
+        boolean result = true;
+        Set<Character> characters1 = new HashSet<>();
+        Set<Character> characters2 = new HashSet<>();
+        for (Character c : str1.toCharArray()) {
+            characters1.add(c);
+        }
+        for (Character c : str2.toCharArray()) {
+            characters2.add(c);
+        }
+
+        return characters1.equals(characters2);
 
     }
 
